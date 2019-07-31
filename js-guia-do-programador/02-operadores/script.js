0895dae338cff23cf5f7bbee8d9a6bd3c6502cb7
Ranie <raniesilvarj@gmail.com>
2019-07-31 01:06:52 -0300

    Add files via upload

--- js-guia-do-programador/02-operadores/script.js
+++ js-guia-do-programador/02-operadores/script.js
@@ -15,65 +15,119 @@ i + 46.8
 ------------------------------------------------------------------------------------------------------------------------------------------------
 [Operadores]
 
-Operador                Finalidade
-+ - * / %               Adição, subtração, multiplicação, divisão, módulo (resto de uma divisão)
-"" + ""                 Concatenar strings
-+ -                     Operador unário
-< > <= =>               Menor que, maior que, maior ou igual, menor ou igual
-++ --                   Pré ou pós incremento, pré ou pós decremento
-== !=                   Teste de igualdade, teste de desigualdade
-=== !==                 Teste de identidade, tesde não identidade
-&& ||                   Concatenação lógica 'e', concatenação lógica 'ou'
-.                       Acessar propriedade de um objeto
-[]                      Indexar arrays
-()                      Chamar função
-?:                      Ternário
-<< >> >>>               Operar bit numérico (bit shifting)
-~                       Operar bit numérico (unário)
-!                       Complemento lógico (unário)
-& | ^                   Operadores de bit AND, OR e XoR
-'                       Separar valores
-=                       Atribuir valor
-+= -= *= /=             Atribuir valor somando, subtraindo, multiplicando, dividindo
-<<= >>= >>>=            Operador de bytes (unário)
-&= |= ^=                Operador de bytes AND, OR e XoR (unário)
-delete                  Tornar indefinida uma propriedade
-in                      Verificar existência de uma propriedade
-instaceof               Verificar o tipo de objeto
-new                     Criar objeto
-typeof                  Retornar o tipo de dado
-void                    Retornar valor indefinido
+Operador                    Finalidade
 */
 
+//1) + - * / %               Adição, subtração, multiplicação, divisão, módulo (resto de uma divisão)
 console.log("5 + 2 -> " + (5 + 2) + "\n5 - 2 -> " + (5 - 2) + "\n5 * 2 -> " + (5 * 2) + "\n5 / 2 -> " + (5 / 2));
+
+//2) "" + ""                 Concatenar strings
 console.log("\"Junt\" + \"ando\" -> " + "Junt" + "ando");
+
+//3) + -                     Operador unário
 console.log("+ com 7 -> " + (+7) + "\n- com 7 -> " + (-7));
+
+//4) < > <= =>               Menor que, maior que, maior ou igual, menor ou igual
 console.log("10 < 8 -> " + (10 < 8) + "\n10 > 8 -> " + (10 > 8) + "\n7 <= 7 -> " + (7 <= 7) + "\n7 >= 7 -> " + (7 >= 7));
+
+//5) ++ --                   Pré ou pós incremento, pré ou pós decremento
 var x = 10;
 console.log("x -> " + x + "\n++x -> " + (++x) + "\nx++ -> " + (x++) + "\nx -> " + x + "\n--x -> " + (--x) + "\nx-- -> " + (x--) + "\nx -> " + x);
+
+//6) == !=                   Teste de igualdade, teste de desigualdade
 console.log("5 == 5 -> " + (5 == 5) + "\n5 == '5' -> " + (5 == '5') + "\n5 != 5 -> " + (5 != '5') + "\n5 != '5' -> " + (5 != '5'));
+
+//7) === !==                 Teste de identidade, tesde não identidade
 console.log("5 === 5 -> " + (5 === 5) + "\n5 === '5' -> " + (5 === '5') + "\n5 !== 5 -> " + (5 !== 5) + "\n5 !== '5' -> " + (5 !== '5'));
+
+//8) && ||                   Concatenação lógica 'e', concatenação lógica 'ou'
 console.log("true && false -> " + (true && false) + "\ntrue || false -> " + (true || false));
+
+//9) .                       Acessar propriedade de um objeto
 var pessoa = new Object();
 pessoa.nome = "Ana";
 console.log(pessoa);
 console.log("pessoa.nome -> " + pessoa.nome);
+
+//10) []                      Indexar arrays
 var time = ["Botafogo"];
 console.log("time[0] -> " + time[0]);
+
+//11) ()                      Chamar função
 var recado = function(){
     return "Volte sempre!";
 };
 console.log("recado() -> " + recado());
+
+//12) ?:                      Ternário
 5 > 2 ? console.log("Argumento 1: true") : console.log("Argumento 2: false");
+
+//13) << >> >>>               Operar bit numérico (bit shifting)
 console.log("5 >> 1 -> " + (5 >> 1) + "\n5 << 1 -> " + (5 << 1) + "\n5 >>> 1 -> " + (5 >>> 1));
 console.log((5..toString(2)) + " >> " + (1..toString(2)) + " -> " + ((5>>2).toString(2)) + "\n" + (5..toString(2)) + " << " + (1..toString(2)) + " -> " + ((5<<2).toString(2)) + "\n" + (5..toString(2)) + " >>> " + (1..toString(2)) + " -> " + ((5>>>2).toString(2)));
+
+//14) ~                       Operar bit numérico (unário)
 console.log("~10 e ~(-10) -> " + ~(10) + " " + ~(-10));
+
+//15) !                       Complemento lógico (unário)
 console.log("!true -> " + !true);
+
+//16) & | ^                   Operadores de bit AND, OR e XoR
 console.log("5 & 1 -> " + (5 & 1) + "\n" + (5..toString(2)) + " & " + (1..toString(2)) + " -> " + ((5 & 1).toString(2)));
 console.log("5 | 1 -> " + (5 & 1) + "\n" + (5..toString(2)) + " | " + (1..toString(2)) + " -> " + ((5 | 1).toString(2)));
 console.log("5 ^ 1 -> " + (5 & 1) + "\n" + (5..toString(2)) + " ^ " + (1..toString(2)) + " -> " + ((5 ^ 1).toString(2)));
+
+//17) ,                       Separar valores
+var carro, bicicleta, motocicleta;
+
+//18) =                       Atribuir valor
+carro = "Ford Focus";
+
+//19) += -= *= /=             Atribuir valor somando, subtraindo, multiplicando, dividindo
 console.log("x += y -> x = x + y");
+console.log("x -= y -> x = x - y");
+console.log("x *= y -> x = x * y");
+console.log("x /= y -> x = x / y");
+
+//20) <<= >>= >>>=            Operador de bytes (unário)
+var x = 10;
+var y = 2;
+console.log("x -> " + x + ", y -> " + y);
+console.log("x >>= y -> x = x >> y -> " + (x >>= y));
+
+//21) &= |= ^=                Operador de bytes AND, OR e XoR (unário)
+console.log("x &= y -> " + (x &= y));
+
+//22) delete                  Tornar indefinida uma propriedade
+var meuObj = {
+    x: 5,
+    y: 7.5,
+    z: 10
+}
+console.log("meuObj.z -> " + meuObj.z);
+delete meuObj.z;
+console.log("meuObj.z -> " + meuObj.z);
+
+//23) in                      Verificar existência de uma propriedade
+console.log("'y' in meuObj -> " + ('y' in meuObj));
+
+//24) instaceof               Verificar o tipo de objeto
+var j = function(){
+    //...
+}
+
+console.log("j instanceof Function -> " + (j instanceof Function));
+
+//25) new                     Criar objeto
+var h = new Object();
+console.log(h);
+
+//26) typeof                  Retornar o tipo de dado
 console.log("typeof boolean -> " + typeof boolean);
+
+//27) void                    Retornar valor indefinido
+console.log("3 -> " + 3);
+console.log("void 3 -> " + void 3);
 
 /*
 > Operandos
@@ -95,6 +149,7 @@ pois antes de invalidar uma operação, os mecanismos internos da linguagem tent
 envolvidos na expressão.
 */
 
+console.log("-------------------------------------\nTipos de Operandos");
 console.log("8 + 4 -> " + (8 + 4) + " -> " + typeof (8 + 4));
 console.log("'8' + '4' -> " + ('8' + '4') + " -> " + typeof ('8' + '4'));
 console.log("'8.56' + 4 -> " + ('8,56' + 4) + " -> " + typeof ('8.56' + 4));
@@ -105,7 +160,6 @@ console.log("'Olá' + true -> " + ('Olá' + true) + " -> " + typeof ('Olá' + tr
 console.log("8 * 'olá' -> " + (8 * 'olá') + " -> " + typeof (8 * 'olá'));
 console.log("8 + 'olá' -> " + (8 + 'olá') + " -> " + typeof (8 + 'olá'));
 console.log("'oi' * 'olá' -> " + ('oi' * 'olá') + " -> " + typeof ('oi' * 'olá'));
-console.log("typeof NaN -> " + typeof NaN);
 
 /*
 > Conversão de dados
@@ -192,105 +246,4 @@ Os operadores Javascript, segundo suas características, podem ser agrupados em 
 - de bits;
 - de atribuição;
 - especiais.
-
-> Operações Matemáticas: Se destinam a realizar operação com valores definidos por variáveis ou valores literais e retornar um valor.
-
-Operador        Operação
-+               Adição
--               Subtração
-*               Multiplicação
-/               Divisão
-%               Módulo
-+               Unário mais
--               Unário menos
-++              Incremento
---              Decremento
-
-> Adição
-O operador binário adição (+) tem dupla finalidade: somar dois operandos ou concatenar duas strings. Quem determina a soma ou concatenação é a natureza dos operandos.
 */
-
-console.log("\nAdição");
-//Se os dois operandos são valores numéricos, a operação é a soma:
-console.log(8 + 3);
-console.log(9.7 + 4);
-
-//Se os dois operandos são strings, a operação é a concatenação
-console.log("Olá " + "Mundo");
-console.log("45.63" + "92");
-
-// Se um operando é string e o outro não string, este é convertido para string e a operação é cancatenação:
-console.log("Olá" + 45.7);
-console.log("Olá" + function(){});
-
-/*
-> Subtração
-O operador binário subtração (-) efetua o cálculo subtraindo a segundo operando do primeiro. Se um ou os dois operando não forem valores 
-numéricos, o operador tentará convertê-los em números.
-*/
-
-console.log("\nSubtração");
-//Se os dois operandos são valores numéricos, a operação é a diferença
-console.log(8 - 3);
-console.log(9.7 - 4);
-
-//Se os dois operandos são string, o resultado é NaN:
-console.log("Olá " - "Mundo!");
-
-//Se os dois operandos são strings numéricas, o resultado é o número diferença:
-console.log("7" - "3");
-
-/*
-> Multiplicação
-O operador binário multiplicação (*) efetua o cálculo multiplicando os dois operandos. Se um ou os dois operandos não forem valores numéricos, 
-o operador tentará convertê-los em números
-*/
-
-console.log("\nMultiplicação");
-//Se os dois operandos são valores numéricos, a operação é a multiplicação:
-console.log(8 * 3);
-console.log(9.7 * 4);
-
-/*
-> Divisão
-O operador binário divisão (/) efetua o cálculo dividindo o primeiro operando pelo segundo operando. Se um ou os dois operandos não forem 
-valores numéricos, o operador tentará convertê-los em números
-*/
-
-console.log("\nDivisão");
-//Se os dois operandos são valores numéricos, a operação é a divisão:
-console.log(8/2);
-console.log(9.6/3);
-
-//Se os dois operandos são strings, o resultado será NaN:
-console.log("Olá "/"Mundo!");
-
-//Se os dois operandos são strings numéricas, o resultado é o número divisão:
-console.log("8"/"2");
-
-/*
-> Módulo
-O operador binário módulo (%) efetua o cálculo dividindo o primeiro operando pelo segundo operando e retornando o resto da divisão. Se um ou dois operandos não forem valores numéricos, o operador tentará convertê-los em números
-*/
-console.log("\nMódulo");
-console.log(3%2);
-console.log(9%5);
-console.log(-7%4);
-console.log(6.7%5);
-
-/*
-> Unário menos
-O operador unário menos (-) destina-se a trocar o sinal do operando ao qual é aplicado, ou seja, converte em negativo um número positivo e vice-versa. Se o operando não for um valor numérico, o operador tentará convertê-lo em número.
-*/
-console.log("\nUnário Menos")
-console.log(-4);
-console.log(-6.87);
-
-/*
-> Unário mais
-O operador unário mais (+) é o simétrico do unário menos e destina-se a declarar explicitamento, o sinalde um operando. Se o operando não for um valor numérico, o operador tentará convertê-lo em número e, como consequência, o uso desse operador em uma string numérica, por exemplo, forçará sua conversão em número.
-*/
-console.log("\nUnário Mais");
-console.log(+1000);
-console.log(+"40");
-
