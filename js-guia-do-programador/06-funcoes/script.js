0aee8cb09e2ac0abfe18be5d12481c488310865c
Ranie <raniesilvarj@gmail.com>
2019-08-06 00:15:19 -0300

    Add files via upload

--- js-guia-do-programador/06-funcoes/script.js
+++ js-guia-do-programador/06-funcoes/script.js
@@ -1,5 +1,5 @@
 /**
- * > FUNÇÕES
+ * > Funções
  * 
  * Função é um bloco de código capaz de realizar ações, como efetuar uma cálculo numérico ou manipular dados e retornar um valor para o 
  * programa principal. 
@@ -15,7 +15,7 @@
  * legibilidade ao programa e facilitar a manutenção. Em programas longos, é comum optar pela criação de um arquivo único contendo todas as 
  * funções utilizadas no script.
  * 
- * > CRIANDO FUNÇÕES
+ * > Criando funções
  * 
  * Existem três sintaxes diferentes para se criar funções em Javascript.
  */
@@ -34,7 +34,7 @@ var minhaFuncao3 = function(){
 };
 
 /**
- * > DECLARAÇÃO FUNCTION
+ * > Declarar function
  * 
  * Funções criadas com o uso dessa sintaxe são denominadas funções estáticas.
  */
@@ -74,4 +74,100 @@ function calculaRetangulo2(b, h){
     console.log("Área: " + area + "\nPerímetro: " + perimetro);
 };
 
-calculaRetangulo2(5, 10);
+calculaRetangulo2(5, 10); //Lembrando que podemos também fazer que o usuário informe os argumentos
+
+/**
+ * > Function()
+ * 
+ * O construtor Function() é nativo da linguagem e destina-se a criar um objeto função.
+ * var minhaFuncao = new Function(["arg1", "arg2", ... , "argn"], "corpo da função");
+ * 
+ * Ao construtor Function() é passada uma lista de strings que são os argumentos da função. A última string é o corpo da função e os argumentos 
+ * anteriores são opcionais. A função é armazenada em uma variável definida pelo desenvolvedor.
+ * 
+ * Funções declaradas com o uso dessa sintaxe são denominadas funções anônimas e dinâmicas.
+ */
+
+var ola3 = new Function(console.log("Bem-vindo ao nosso site."));
+console.log(ola3);
+ola3();
+
+var ola4 = new Function("var nomeVisitante = prompt('Por favor, digite seu nome: ', '');" +
+    "nomeVisitante = (nomeVisitante) ? nomeVisitante : 'Visitante';" +
+    "console.log('Bem-vindo ao nosso site: ' + nomeVisitante);"
+);
+
+ola4();
+
+var calculaAreaRetangulo = new Function("return b * h;");
+
+console.log(calculaAreaRetangulo(b = 10, h = 4));
+
+/**
+ * > Sintaxe Literal
+ * 
+ * Funções criadas com a sintaxe literal são também conhecidas como funções expressões. É armazenada em uma variável defina pelo desenvolvedor.
+ * Funções declaradas com o uso dessa sintaxe são denominadas funções anônimas.
+ */
+
+var ola5 = function(){
+    return "Bem-vindo ao meu site.";
+};
+
+console.log(ola5());
+
+var ola6 = function(){
+    var nomeVisitante = prompt("Nome: ", "");
+    nomeVisitante = (nomeVisitante) ? nomeVisitante : "Curioso";
+    return "Bem-vindo " + nomeVisitante;
+};
+
+console.log(ola6());
+
+var calculaAreaRetangulo2 = function(b, h){
+    return b * h;
+};
+
+calculaAreaRetangulo2(8, 7);
+
+
+//> Retornando mais de um valor
+function calculaRetangulo3(b, h){
+    var area = b * h;
+    var perimetro = (b + h) * 2;
+    return area;
+    return perimetro; //Não vai funcionar
+};
+
+function calculaRetangulo4(b, h){
+    var area = b * h;
+    var perimetro = (b + h) * 2;
+    return [area, perimetro];
+};
+
+var resultados = calculaRetangulo4(3, 8);
+console.log(typeof resultados);
+console.log(resultados);
+console.log(resultados[0]);
+
+//> Retornando objeto
+function calculaRetangulo5(b, h){
+    var area = b * h;
+    var perimetro = (b + h) * 2;    
+    return {
+        a: area,
+        p: perimetro
+    };
+};
+
+var resultados2 = calculaRetangulo5(8, 4);
+console.log(typeof resultados2);
+console.log(resultados2);
+console.log(resultados2['a']);
+
+//Passagem por referência
+var referencia = calculaRetangulo5;
+console.log(referencia(2, 6));
+
+
+//> Escopo de uma Função
